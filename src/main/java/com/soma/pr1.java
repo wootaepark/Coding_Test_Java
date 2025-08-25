8b1559a26e08ea30278f9d1f5faa9e3b0c54a796
wootaepark <pakdal99@gmail.com>
2025-08-25 12:16:29 +0900

    init commit

---
+++ src/main/java/com/soma/pr1.java
@@ -0,0 +1,14 @@
+package com.soma;
+
+import java.util.Scanner;
+
+public class pr1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("Hello World");
+		System.out.println(a);
+	}
+}
